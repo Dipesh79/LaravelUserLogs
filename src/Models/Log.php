505f7ff88fc057e9f8bd4a2135330545a33af074
Achyut Neupane <30431426+achyutkneupane@users.noreply.github.com>
2023-08-22 21:20:13 +0545

    Revert "Default Add method with action as parameter"

--- src/Models/Log.php
+++ src/Models/Log.php
@@ -2,52 +2,19 @@
 
 namespace Dipesh79\LaravelUserLogs\Models;
 
-class Log extends \Illuminate\Database\Eloquent\Model
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Log extends Model
 {
-    /**
-     * This property defines the fields that are not mass assignable.
-     *
-     * @var array
-     */
     protected $guarded = [];
 
-    /**
-     * This property defines the table name for the model.
-     *
-     * @var string
-     */
     protected $table = 'user_activity_logs';
 
-    /**
-     * This property defines the timestamps for the model.
-     *
-     * @var bool
-     */
     public $timestamps = true;
 
-    /**
-     * This method defines the relationship between the Log model and the model that is being logged.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
-     */
-    public function loggable(): \Illuminate\Database\Eloquent\Relations\MorphTo
+    public function loggable(): MorphTo
     {
         return $this->morphTo();
-    }
-
-    /**
-     * This method is used to add a new log with default values. It can be used to add a log for any action.
-     *
-     * @param string $action
-     * @return \Illuminate\Database\Eloquent\Model
-     */
-    public function addLog($action = 'create'): \Illuminate\Database\Eloquent\Model
-    {
-        return $this->create([
-            'action' => $action,
-            'ip_address' => request()->ip(),
-            'device' => request()->userAgent(),
-            'user_id' => auth()->user()->id
-        ]);
     }
 }
